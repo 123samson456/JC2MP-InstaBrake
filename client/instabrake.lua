da6c6ff49112597d104a5e4e22f5c5a2ab4c1446
123samson456 <breitung.programming@gmx.de>
2014-07-16 17:45:12 +0200

    Fixed a Bug
    Fixed a Bug that allowed passengers using the instabrake

--- client/instabrake.lua
+++ client/instabrake.lua
@@ -2,13 +2,16 @@ class 'InstaBrake'
 
 function InstaBrake:__init()
     Events:Subscribe("KeyDown", self, self.KeyDown)
-    self.topress = "Y"
+    self.topress = string.byte("Y")
 end
 
 function InstaBrake:KeyDown(args)
     if LocalPlayer:InVehicle() then
-        if (args.key == string.byte(self.topress)) then
-            Network:Send("instabrake",true)
+        local veh = LocalPlayer:GetVehicle()
+        if veh:GetDriver() == LocalPlayer then
+            if (args.key == self.topress) then
+                Network:Send("instabrake",true)
+            end
         end
     end
 end
